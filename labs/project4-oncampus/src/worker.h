da5246a6f92edc4dbe752aac8c81a64c86e6201f
GTK <gaurav21776@gmail.com>
2020-04-14 01:20:51 -0400

    master worker services addded

--- labs/project4-oncampus/src/worker.h
+++ labs/project4-oncampus/src/worker.h
@@ -2,7 +2,32 @@
 
 #include <mr_task_factory.h>
 #include "mr_tasks.h"
+#include <grpcpp/grpcpp.h>
+#include "masterworker.grpc.pb.h"
 
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using masterworker::File;
+using masterworker::FileShard;
+using masterworker::MapQuery;
+using masterworker::MasterWorker;
+
+class MapperServiceImpl final : public MasterWorker::Service{
+	Status mapper(ServerContext* context, const MapQuery* request, File* reply){
+		// mapper implementation
+		return Status::OK;
+	}
+};
+
+class ReducerServiceImpl final : public MasterWorker::Service{
+	
+	Status reducer(ServerContext* context, const File* request, File* reply){
+		// reducer implementation
+		return Status::OK;
+	}
+};
 
 /* CS6210_TASK: Handle all the task a Worker is supposed to do.
 	This is a big task for this project, will test your understanding of map reduce */
@@ -16,6 +41,10 @@ class Worker {
 		bool run();
 
 	private:
+		ServerBuilder builder;
+		MapperServiceImpl map_service;
+		ReducerServiceImpl red_service;
+
 		/* NOW you can add below, data members and member functions as per the need of your implementation*/
 
 };
@@ -24,6 +53,7 @@ class Worker {
 /* CS6210_TASK: ip_addr_port is the only information you get when started.
 	You can populate your other class data members here if you want */
 Worker::Worker(std::string ip_addr_port) {
+	builder.AddListeningPort(ip_addr_port, grpc::InsecureServerCredentials());
 
 }
 
@@ -38,10 +68,20 @@ extern std::shared_ptr<BaseReducer> get_reducer_from_task_factory(const std::str
 bool Worker::run() {
 	/*  Below 5 lines are just examples of how you will call map and reduce
 		Remove them once you start writing your own logic */ 
+	
+	builder.RegisterService(&map_service);
+	builder.RegisterService(&red_service);
+
+	std::unique_ptr<Server> server(builder.BuildAndStart());
+
+	server->Wait();
+	/*
 	std::cout << "worker.run(), I 'm not ready yet" <<std::endl;
 	auto mapper = get_mapper_from_task_factory("cs6210");
 	mapper->map("I m just a 'dummy', a \"dummy line\"");
 	auto reducer = get_reducer_from_task_factory("cs6210");
 	reducer->reduce("dummy", std::vector<std::string>({"1", "1"}));
+	
+	*/
 	return true;
 }
